5044db2373d928698e8213db1fdbc6e8ff5c6714
Kamila Kaioana Aleixo Costa <kamila.costa@db1.com.br>
2022-05-06 18:26:31 -0300

    adiciona template react

--- BACKEND/src/services/livroService.js
+++ BACKEND/src/services/livroService.js
@@ -6,10 +6,7 @@ Livro.methods(["get", "post", "put", "delete"]);
 
 Livro.updateOptions({new: true, runValidators: true})
 
-Livro.before("get", isAuthenticated)
-Livro.before("post", isAuthenticated)
-Livro.before("put", isAuthenticated)
-Livro.before("delete", isAuthenticated)
+Livro.before("get", isAuthenticated).before("post", isAuthenticated).before("put", isAuthenticated).before("delete", isAuthenticated);
 
 Livro.after("post", errorHandler).after("put", errorHandler)
 
---
+++ FRONTEND/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './screens/login';
+
+function App() {
+  return (
+
+    <Login />  
+  );
+}
+
+export default App;
