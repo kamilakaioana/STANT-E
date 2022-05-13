8dc86bd4f52dc84f9a47ed23001734bec8a44e38
Kamila Kaioana Aleixo Costa <kamila.costa@db1.com.br>
2022-05-12 22:11:56 -0300

    config cors

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const port = process.env.PORT || 3004; 
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cors())
 
 
 require("./config/routes")(server);
