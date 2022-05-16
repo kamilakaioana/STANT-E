1a545a4f7bad3b11dae3b0df3670d7c77c9e78db
Kamila Kaioana Aleixo Costa <kamila.costa@db1.com.br>
2022-05-15 22:23:11 -0300

    creat utils project

---
+++ FRONTEND/src/utils/constants.tsx
@@ -0,0 +1,8 @@
+
+  export const Media ={
+    movelMax : 480,
+    tabletMax : 768,
+    screenSmallMax : 1024,
+    screenMediumMax : 1200,
+    screenLargeMin: 1201,
+  }
---
+++ FRONTEND/src/utils/stringsResources.ts
@@ -0,0 +1,18 @@
+export const StringsResources = {
+  screens: {
+    bookRegister:{
+      ADICIONAR_FAVORITOS: "Adicionar favoritos",
+      LIVROS_LIDOS: "Livros lidos",
+      EDITAR: "Editar",
+      EXCLUIR: "Excluir",
+      SALVAR: "Salvar",
+    }
+  },
+  header: {
+    HOME: "Principal",
+    FAVORITOS: "Favoritos",
+    LIDOS: "Lidos",
+    CADASTRAR: "Cadastrar",
+    SAIR: "Sair",
+  }
+}
