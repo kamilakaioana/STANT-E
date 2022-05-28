550f546e91a43b56c8e0f1a602bcc01aca8f1b07
Kamila Kaioana Aleixo Costa <kamila.costa@db1.com.br>
2022-05-28 20:24:28 -0300

    changes logout redirect

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
-import { BrowserRouter,  } from "react-router-dom";
+import { BrowserRouter, useNavigate,  } from "react-router-dom";
 import AppRoutes from './routes'
 import { AuthProvider } from './hooks/auth';
+import { customIntercept } from './services/api';
 
+function AxiosNavigateSetup() {
+  const navigate = useNavigate();
+  customIntercept(navigate)
+  return <></>
+  
+}
 function App() {
   return (
 
         <BrowserRouter>
+        <AxiosNavigateSetup/>
           <AuthProvider>
             <AppRoutes />
           </AuthProvider>
