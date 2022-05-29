6b7cca65dd0e28f676c961962ebca49f57bab599
Kamila Kaioana Aleixo Costa <kamila.costa@db1.com.br>
2022-05-28 21:14:20 -0300

    create component loading  modal

---
+++ FRONTEND/src/components/modais/Loading/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const ModalText = styled.text`
+font-size: 20px;
+font-weight: lighter;
+text-align: center;
+margin-top: 24px;
+letter-spacing: 0.22px;
+  `;
